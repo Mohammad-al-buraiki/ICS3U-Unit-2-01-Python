e3667582c7895d55f0d23e4415b5f3f42fa63244
Ubuntu <ubuntu@ip-172-31-5-27.ec2.internal>
2020-12-11 15:46:26 +0000

    area_and_perimeter_of_a_circle.py

--- area_and_perimeter_of_a_circle.py
+++ area_and_perimeter_of_a_circle.py
@@ -10,15 +10,14 @@ import math
 
 def main():
     # this function calculates the area and the perimeter of a circle
-    
+
     print("If the circle has the radius")
     print("15 mm")
     print("The area of the circle is {} mm²."
     .format(math.pi*(15**2)))
     print("The perimeter of the circle is {} mm."
     .format(2*math.pi*(15)))
-    
-    
+
+
 if __name__ == "__main__":
     main()
-    
